d6c4a3f331d824734571a1e9e1586f7b11be3e0c
geekymoose <constantin.masson@umontreal.ca>
2017-06-19 18:50:42 -0400

    Create CMake configuration to use GoogleTest (Unit Tests)

---
+++ test/math/VectF4_Test.cpp
@@ -0,0 +1,7 @@
+#include "gtest/gtest.h"
+#include "math/VectF4.h"
+
+TEST(Constructor, Empty){
+    VectF4 v;
+    ASSERT_EQ(0,0) << "TODO";
+}
---
+++ test/runAllTests.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "gtest/gtest.h"
+
+GTEST_API_ int main(int argc, char **argv) {
+    printf("Running main() from runAllTests.cpp\n");
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
